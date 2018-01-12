f87f02b1dc9a635f33b809c4fdc249dba715a082
Bruno Hürzeler <bruno.huerzeler@students.fhnw.ch>
2018-01-12 15:13:49 +0100

    Final clean up

--- ebs2WDMiniQ/ebs2WDMiniQ/ebs2WDMiniQ.ino
+++ ebs2WDMiniQ/ebs2WDMiniQ/ebs2WDMiniQ.ino
@@ -1,19 +1,14 @@
 /**********************************************************************************************************************
-FHNW - EMBEDDED SYSTEMS 
+FHNW - EMBEDDED SYSTEMS - MAIN PROGRAM
 **********************************************************************************************************************
 /*  Program:    ebs2WDMiniQ.ino             */
 /*  Revision:   1.0                         */
 /*  Author:     A. Gessler / B. Hürzeler    */
 /*  Date:       2018-01-12                  */
 /**********************************************************************************************************************/
-/* MAIN PROGRAM
-***********************************************************************************************************************/
-
-/****************************************************************
-INCLUDES
-*****************************************************************/
+/*INCLUDES
+/*****************************************************************/
 #include "functions.h"
-
 /****************************************************************
 FUNCTIONS
 *****************************************************************/
@@ -27,31 +22,22 @@ unsigned short fgetKeyValue(tstUI *pstUI);
 void fUIProcedure(tstPrvMain *pstPrivate);
 void fsetUIMenu(tstPrvMain *pstPrivate);
 void fDisplayProcedure(tstUI *pstDisplay);
-
-/****************************************************************
-MEMORY ALLOCATION
-*****************************************************************/
+/*****************************************************************/
 tstPrvMain stPrivate;   //Allocate private memory
-tstUI *pstUI = &stPrivate.stUI;
 
-// the setup function runs once when you press reset or power the board
 void setup() 
 {
-    // Set whole privat memory 0
-    memset(&stPrivate, 0, sizeof(stPrivate));
-    // Call init function
-    finitUp(&stPrivate);
+    memset(&stPrivate, 0, sizeof(stPrivate));   // Set whole privat memory 0
+    finitUp(&stPrivate);                        // Call init function
 }
 
-// the loop function runs over and over again until power down or reset
 void loop() 
 {
-    /*CYCLIC FUNCTIONS*/
     fUIProcedure(&stPrivate);
     fgetLight(&stPrivate.stLight);
     fgetAngle(&stPrivate.stCompass);
     fMoveProcedure(&stPrivate.stMotor);
     fsetUIMenu(&stPrivate);
-    fDisplayProcedure(pstUI);
+    fDisplayProcedure(&stPrivate.stUI);
     fsetColor(&stPrivate);
 }
